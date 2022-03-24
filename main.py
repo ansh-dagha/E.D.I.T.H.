ee59fca5c99284f752f7a5689165f8035add66a5
ansh-dagha <ansh.dagha@somaiya.edu>
2022-03-24 10:38:06 +0530

    Weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 import webbrowser
+import requests
 from utilities.websearch import search_for,youtube
 from utilities.speech_functions import *
 from utilities.email_ import *
@@ -29,22 +30,22 @@ while True:
     if statement == None:
         continue
 
-    if "hello friday" in statement or 'hey' in statement or 'hello' in statement:
+    if "hello edith" in statement or 'hey' in statement or 'hello' in statement:
         speak('Oh Hello sir')
 
     elif 'time' in statement:
         strTime = datetime.datetime.now().strftime("%I:%M:%p")
         speak(f"It\'s {strTime} right now")
 
-    if 'email' in statement:
+    elif 'email' in statement:
         sendEmail()
     
-    if 'search' in statement:
+    elif 'search' in statement:
         param = statement.replace("search", "")
         search_for(param)
 
-    # elif 'snapshot' in statement:
-    #     snapshot()
+    elif 'snapshot' or 'capture' or 'snap' or 'snip' in statement:
+        snapshot()
     
     elif 'log off' in statement:
         if confirm():
@@ -65,3 +66,28 @@ while True:
     elif 'play on youtube' in statement:
         param = statement.replace("play on youtube ", "")
         youtube(param)
+
+    elif "weather" in statement:
+        api_key = "8ef61edcf1c576d65d836254e11ea420"
+        base_url = "https://api.openweathermap.org/data/2.5/weather?"
+        speak("Which city sir?")
+        city_name = listen()
+        complete_url = base_url+"appid="+api_key+"&q="+city_name
+        response = requests.get(complete_url)
+        x = response.json()
+        if x["cod"] != "404":
+            y = x["main"]
+            temperature = y["temp"] - 273.15
+            temperature = round(temperature)
+            humidity = y["humidity"]
+            z = x["weather"]
+            weather_description = z[0]["description"].capitalize()
+            print(f'Temperature = {temperature} C \nHumidity = {humidity}% \nDescription: {weather_description}')
+            speak(f'It\'s {temperature} degree celsius and {weather_description} \n Humidity is {humidity}%')
+
+        else:
+            speak("Sorry City Not Found!")
+
+    # elif 'bye' or 'goodbye' in statement:
+    #     speak('See you soon Sir!')
+    #     break
