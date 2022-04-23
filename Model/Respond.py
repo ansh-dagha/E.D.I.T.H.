06e643ec52c1045632d4bc16523dc947c7d835aa
Gayatri Patil <78681507+GayatriVP@users.noreply.github.com>
2022-04-23 12:33:45 +0530

    Model done

---
+++ Model/Respond.py
@@ -0,0 +1,173 @@
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+import random
+import datetime
+# from googlesearch import *
+import webbrowser
+import requests
+from utilities.speech_functions import *
+from utilities.websearch import search_for,youtube,checkconn
+from utilities.email_ import *
+from utilities.powerOptions import *
+from utilities.confirm import *
+# from utilities.capture import *
+import billboard
+import time
+from pygame import mixer
+
+from keras.models import load_model
+model = load_model('Model/chatbot_model.h5')
+import json
+import random
+intents = json.loads(open('Model/intents.json').read())
+words = pickle.load(open('Model/words.pkl','rb'))
+classes = pickle.load(open('Model/classes.pkl','rb'))
+
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getResponse(return_list, intents_json):
+    if len(return_list) == 0:
+        tag = 'noanswer'
+    else:
+        tag = return_list[0]['intent']
+    
+    if tag=='datetime':        
+        strTime = datetime.datetime.now().strftime("%I:%M:%p")
+        print(f"It\'s {strTime} right now")
+        speak(f"It\'s {strTime} right now")
+
+    if tag=='email':
+        if checkconn():
+            sendEmail()
+
+    if tag == 'youtube':
+        if checkconn():
+            print("What should I play?")
+            param = listen()
+            youtube(param)
+
+    if tag=='google':
+        if checkconn():
+            print("What should I search for?")
+            speak("What should I search for?")
+            statement = listen()
+            param = statement.replace("search", "")
+            search_for(param)
+    
+    if tag=='weather':
+        if checkconn():
+            api_key = "8ef61edcf1c576d65d836254e11ea420"
+            base_url = "https://api.openweathermap.org/data/2.5/weather?"
+            speak("Which city sir?")
+            city_name = listen()
+            complete_url = base_url+"appid="+api_key+"&q="+city_name
+            response = requests.get(complete_url)
+            x = response.json()
+            if x["cod"] != "404":
+                y = x["main"]
+                temperature = y["temp"] - 273.15
+                temperature = round(temperature)
+                humidity = y["humidity"]
+                z = x["weather"]
+                weather_description = z[0]["description"].capitalize()
+                print(f'Temperature = {temperature} C \nHumidity = {humidity}% \nDescription: {weather_description}')
+                speak(f'It\'s {temperature} degree celsius and {weather_description} \n Humidity is {humidity}%')
+
+            else:
+                speak("Sorry City Not Found!")
+
+    if tag == 'news':
+        main_url = " http://newsapi.org/v2/top-headlines?country=in&apiKey=bc88c2e1ddd440d1be2cb0788d027ae2"
+        open_news_page = requests.get(main_url).json()
+        article = open_news_page["articles"]
+        results = [] 
+          
+        for ar in article: 
+            results.append([ar["title"],ar["url"]]) 
+          
+        for i in range(10): 
+            print(i + 1, results[i][0])
+            print(results[i][1],'\n')
+    
+    if tag=='song':
+        chart=billboard.ChartData('hot-100')
+        print('The top 10 songs at the moment are:')
+        for i in range(10):
+            song=chart[i]
+            print(song.title,'- ',song.artist)
+    
+    if tag=='timer':        
+        mixer.init()
+        x=input('Minutes to timer..')
+        time.sleep(float(x)*60)
+        mixer.music.load('Handbell-ringing-sound-effect.mp3')
+        mixer.music.play()
+
+    list_of_intents= intents_json['intents']    
+    for i in list_of_intents:
+        if tag==i['tag']:
+            result = random.choice(i['responses'])
+    return result
+
+def assis_response(msg):
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
+
+
+def learn():
+    print('Help me Learn?')
+    tag=input('Please enter general category of your question  ')
+    flag=-1
+    for i in range(len(intents['intents'])):
+        if tag.lower() in intents['intents'][i]['tag']:
+            intents['intents'][i]['patterns'].append(input('Enter your message: '))
+            intents['intents'][i]['responses'].append(input('Enter expected reply: '))        
+            flag=1
+
+    if flag==-1:
+        
+        intents['intents'].append (
+            {'tag':tag,
+            'patterns': [input('Please enter your message')],
+            'responses': [input('Enter expected reply')]})
+        
+    with open('intents.json','w') as outfile:
+        outfile.write(json.dumps(intents,indent=4))
--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from utilities.speech_functions import *
 from utilities.email_ import *
 from utilities.powerOptions import *
 from utilities.confirm import *
-from utilities.capture import *
+# from utilities.capture import *
+from Model.Respond import *
 
 gender = ['Female', 'Male']
 addressee = ['Sir', 'Miss', 'Boss']
@@ -23,83 +24,83 @@ def greet(addressee):
 
 greet(addressee[2])
 
-WAKE = "hey tim"
-# if checkconn():
-#     # param = statement.replace("search", "")
-#     search_for('taj mahal')
-# else:
-#     print('no search')
-while True:
+WAKE = "hey edith"
+statement='list the top 10 songs'
+res = assis_response(statement)
+print(res)
+
+# while True:
     
-    statement = listen()
+    # statement = listen()
     
-    if statement == None:
-        continue
-
-    if "hello edith" in statement or 'hey' in statement or 'hello' in statement:
-        speak('Oh Hello sir')
-
-    elif 'time' in statement:
-        strTime = datetime.datetime.now().strftime("%I:%M:%p")
-        print(f"It\'s {strTime} right now")
-        speak(f"It\'s {strTime} right now")
-
-    elif 'email' in statement:
-        if checkconn():
-            sendEmail()
     
-    elif 'search' in statement:
-        if checkconn():
-            param = statement.replace("search", "")
-            search_for(param)
+    # if statement == None:
+    #     continue
 
-    elif 'snapshot' in statement or 'snip' in statement or 'snap' in statement or 'screenshot' in statement:
-        snapshot()
+    # if "hello edith" in statement or 'hey' in statement or 'hello' in statement:
+    #     speak('Oh Hello sir')
+
+    # elif 'time' in statement:
+    #     strTime = datetime.datetime.now().strftime("%I:%M:%p")
+    #     print(f"It\'s {strTime} right now")
+    #     speak(f"It\'s {strTime} right now")
+
+    # elif 'email' in statement:
+    #     if checkconn():
+    #         sendEmail()
     
-    elif 'log off' in statement:
-        if confirm():
-            execute("shutdown /l")
+    # elif 'search' in statement:
+    #     if checkconn():
+    #         param = statement.replace("search", "")
+    #         search_for(param)
+
+    # elif 'snapshot' in statement or 'snip' in statement or 'snap' in statement or 'screenshot' in statement:
+    #     snapshot()
     
-    elif 'shutdown' in statement:
-        if confirm():
-            execute("shutdown /s")
+    # elif 'log off' in statement:
+    #     if confirm():
+    #         execute("shutdown /l")
     
-    elif 'sleep' in statement:
-        if confirm():
-            execute("rundll32.exe powrprof.dll,SetSuspendState Sleep")
-
-    elif 'open youtube' in statement:
-        if checkconn():
-            webbrowser.open_new_tab("https://www.youtube.com")
-            speak("Youtube is open now")
+    # elif 'shutdown' in statement:
+    #     if confirm():
+    #         execute("shutdown /s")
     
-    elif 'play on youtube' in statement:
-        if checkconn():
-            param = statement.replace("play on youtube ", "")
-            youtube(param)
+    # elif 'sleep' in statement:
+    #     if confirm():
+    #         execute("rundll32.exe powrprof.dll,SetSuspendState Sleep")
 
-    elif "weather" in statement:
-        if checkconn():
-            api_key = "8ef61edcf1c576d65d836254e11ea420"
-            base_url = "https://api.openweathermap.org/data/2.5/weather?"
-            speak("Which city sir?")
-            city_name = listen()
-            complete_url = base_url+"appid="+api_key+"&q="+city_name
-            response = requests.get(complete_url)
-            x = response.json()
-            if x["cod"] != "404":
-                y = x["main"]
-                temperature = y["temp"] - 273.15
-                temperature = round(temperature)
-                humidity = y["humidity"]
-                z = x["weather"]
-                weather_description = z[0]["description"].capitalize()
-                print(f'Temperature = {temperature} C \nHumidity = {humidity}% \nDescription: {weather_description}')
-                speak(f'It\'s {temperature} degree celsius and {weather_description} \n Humidity is {humidity}%')
+    # elif 'open youtube' in statement:
+    #     if checkconn():
+    #         webbrowser.open_new_tab("https://www.youtube.com")
+    #         speak("Youtube is open now")
+    
+    # elif 'play on youtube' in statement:
+    #     if checkconn():
+    #         param = statement.replace("play on youtube ", "")
+    #         youtube(param)
 
-            else:
-                speak("Sorry City Not Found!")
+    # elif "weather" in statement:
+    #     if checkconn():
+    #         api_key = "8ef61edcf1c576d65d836254e11ea420"
+    #         base_url = "https://api.openweathermap.org/data/2.5/weather?"
+    #         speak("Which city sir?")
+    #         city_name = listen()
+    #         complete_url = base_url+"appid="+api_key+"&q="+city_name
+    #         response = requests.get(complete_url)
+    #         x = response.json()
+    #         if x["cod"] != "404":
+    #             y = x["main"]
+    #             temperature = y["temp"] - 273.15
+    #             temperature = round(temperature)
+    #             humidity = y["humidity"]
+    #             z = x["weather"]
+    #             weather_description = z[0]["description"].capitalize()
+    #             print(f'Temperature = {temperature} C \nHumidity = {humidity}% \nDescription: {weather_description}')
+    #             speak(f'It\'s {temperature} degree celsius and {weather_description} \n Humidity is {humidity}%')
 
-    elif 'bye' in statement or 'quit' in statement or 'goodbye' in statement:
-        speak('See you soon Sir!')
-        break
+    #         else:
+    #             speak("Sorry City Not Found!")
+
+    # elif 'bye' in statement or 'quit' in statement or 'goodbye' in statement:
+    #     speak('See you soon Sir!')
+    #     break
