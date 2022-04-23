9291bd120aeefa812f60eb7b34d6744916fe7c61
MihirHundiwala <mihir.hundiwala@somaiya.edu>
2022-04-23 17:41:28 +0530

    ToDoList with notifications and open file

--- utilities/get_date_time.py
+++ utilities/get_date_time.py
@@ -7,15 +7,14 @@ DAY_EXTENTIONS = ["rd", "th", "st", "nd"]
 
 def get_date(text):
     text = text.lower()
+    today = datetime.datetime.today()
 
     y = re.search('\d{4}',text)
     if y:
+        text = re.sub(y.group(0), '', text)
         y = y.group(0)
     else:
         y = ''
-    text = re.sub(y, '', text)
-
-    today = datetime.date.today()
 
     if text.count("today") > 0:
         return today
@@ -61,8 +60,8 @@ def get_date(text):
             dif += 7
             if text.count("next") >= 1:
                 dif += 7
-
-        return today + datetime.timedelta(dif)
+        res = today + datetime.timedelta(dif)
+        return res.strftime('%d'), res.strftime('%m'), res.strftime('%B'), res.strftime('%Y')
 
     try:
         month_in_words = MONTHS[month-1]
@@ -70,8 +69,7 @@ def get_date(text):
         month_in_words = month
 
     if day != -1:  # FIXED FROM VIDEO
-        return day, month, month_in_words, int(y) or year
-
+        return str(day).zfill(2), str(month).zfill(2), month_in_words, str(y) or str(year)
 
 def get_time(text):
     # returns hours, minutes, 'AM' or 'PM'
@@ -79,7 +77,30 @@ def get_time(text):
         text = text.lower()
         text = text.replace(':',' ').replace('p.m.','PM').replace('a.m.','AM').replace('am','AM').replace('pm','PM')
 
-        text = text.split()
-        return int(text[0]), int(text[1]), text[2]
-    except:
-        return 12, 0, 'PM'
+        res = list(map(int ,re.findall("\d+", text)))
+        if len(res) >= 2:
+            hours = (res[0] + int(res[1]/60))%12
+            minutes = res[1]%60
+        else:
+            hours = res[0]%12
+            minutes = 0
+            
+        f = re.search('PM|AM', text)
+        if f:
+            f = f.group(0)
+        else:
+            f='AM'
+        
+        return str(hours).zfill(2), str(minutes).zfill(2), f, '0' if f=='AM' else '1'
+    except Exception as e:
+        return '11', '59', 'AM', '0'
+
+# print(get_date('monday'))
+# print(get_date('next monday'))
+# print(get_date('15 june 2022'))
+# print(get_date('31 february 2021'))
+
+# print(get_time('4:45 p.m.'))
+# print(get_time('4 p.m.'))
+# print(get_time('4 02 p.m.'))
+# print(get_time('4 02 '))
--- utilities/todolist.py
+++ utilities/todolist.py
@@ -1,46 +1,51 @@
 import os, sys, json, re
-from datetime import datetime
+from datetime import datetime as dt
 from utilities.get_date_time import *
 from utilities.speech_functions import * 
 
 
-users_dir = os.path.join(os.path.dirname(sys.path[0]),'users')
+users_dir = os.path.join(os.path.dirname(sys.path[0]),'todolists')
 os.makedirs(users_dir, exist_ok=True)
 
 
-def preprocess(task, reminder_date, time):
+def preprocess(task, due_date, reminder_date, time):
     try:
         if (task == "" or task == None):
             task = "Do Nothing"
+        
+        try:
+            day, month, month_in_words, year = get_date(due_date)
+            due_date = f"{int(day)} {month_in_words}"
+            dueSortKey = year+month+day
+        except Exception as e:
+            print(e)
+            due_date = "No Due Date"
+            dueSortKey = "99991231"
+
         try:
             day, month, month_in_words, year = get_date(reminder_date)
         except Exception as e:
             print(e)
-            reminder_date = (datetime.now().day,
-                        datetime.now().month,
-                        datetime.now().year)
-        hours, minutes, f = get_time(time)
+            today = dt.today()
+            day, month, month_in_words, year = (str(today.day).zfill(2), str(today.month).zfill(2), today.strftime("%B"), str(today.year))
 
-        reminder_date = f"{day} {month_in_words} {year}"
-        time = f"{hours} {minutes} {f}"
-        dtKey = str(year)+str(month).zfill(2)+str(day).zfill(2)+f+str(hours).zfill(2)+str(minutes).zfill(2)
-
-        return task, reminder_date, time, dtKey
+        hours, minutes, f_in_words, f = get_time(time)
+        reminder = int(year+month+day+f+hours+minutes)
+        return task, due_date, dueSortKey, reminder
     
     except Exception as e:
         print(e)
         pass
     
 
-
-def add_to_list(username, task, reminder_date, time):
+def add_to_list(username, task, due_date, reminder_date, time):
     try:
-        task, reminder_date, time, dtKey = preprocess(task, reminder_date, time)
+        task, due_date, dueSortKey, reminder = preprocess(task, due_date, reminder_date, time)
 
         tasks = []
-        open(f"{users_dir}\\{username}.json", 'a').close()
+        open(f"{users_dir}\\{username}\\{username}.json", 'a').close()
 
-        with open(f"{users_dir}\\{username}.json") as fileobj:
+        with open(f"{users_dir}\\{username}\\{username}.json") as fileobj:
             try:
                 tasks = json.load(fileobj)
             except Exception as e:
@@ -49,15 +54,15 @@ def add_to_list(username, task, reminder_date, time):
 
             task_obj = {
                 'task':task,
-                'reminder_date': reminder_date,
-                'time':time,
-                'dtKey': dtKey,
+                'due_date': due_date,
+                'dueSortKey': dueSortKey,
+                'reminder': reminder,
             }
             tasks.append(task_obj)
 
-            tasks.sort(key = lambda d: d['dtKey'])
+            tasks.sort(key = lambda d: d['reminder'])
 
-        with open(f"{users_dir}\\{username}.json",'w') as fileobj:
+        with open(f"{users_dir}\\{username}\\{username}.json",'w') as fileobj:
             fileobj.write(json.dumps(tasks))
 
         speak('Task added to your list')
@@ -71,44 +76,69 @@ def remove_from_list(username, task_no):
     try:
         tasks = []
 
-        open(f"{users_dir}\\{username}.json", 'a').close()
-
-        with open(f"{users_dir}\\{username}.json") as fileobj:
+        with open(f"{users_dir}\\{username}\\{username}.json") as fileobj:
             try:
                 tasks = json.load(fileobj)
             except Exception as e:
                 print(e)
-                tasks = []
-                pass
+                speak('No pending tasks')
+                return
         
         if task_no > len(tasks) or task_no < 1:
             speak("Invalid Task Number")
             return
-
-        with open(f"{users_dir}\\{username}.json",'w') as fileobj:
+        
+        tasks.pop(task_no-1)
+        
+        with open(f"{users_dir}\\{username}\\{username}.json",'w') as fileobj:
             fileobj.write(json.dumps(tasks))
         
         speak('Task removed from your list')
+
     except Exception as e:
         print(e)
 
 
-def open_list(username):
+def to_do_list_open(username):
+    output = "No pending tasks"
 
-    pass
+    try:
+        with open(f"{users_dir}\\{username}\\{username}.json") as fileobj:
+            tasks = json.load(fileobj)
+            if len(tasks) == 0:
+                print(output)
+                return output
+            output = ""
+            tasks.sort(key = lambda task: task['dueSortKey'])
+            for taskObj in tasks:
+                if taskObj['due_date'] != 'No Due Date':
+                    output += f"Task : "+ taskObj['task'] + " due on " + taskObj['due_date'] + "\n"
+                else:
+                    output += f"Task : "+ taskObj['task'] + " is not due" + "\n"
+    
+        with open(f"{users_dir}\\tasks.txt",'w') as fileobj:
+            fileobj.write(output)
 
+        os.startfile(f"{users_dir}\\tasks.txt")
+        speak(output)
+
+    except Exception as e:
+        speak('Unable to access to do list')
+        print(e)
 
 
 def to_do_list_add_task(username):
     try:
         speak('What task do you want me to add')
         task = listen()
+        speak('When is the task due?')
+        due_date = listen()
         speak('On which date should i remind you')
         reminder_date = listen()
         speak('On What time should i remind you')
         reminder_time = listen()
 
-        add_to_list(username, task, reminder_date, reminder_time)
+        add_to_list(username, task, due_date, reminder_date, reminder_time)
     except Exception as e:
         speak('Sorry unable to add task')
     
@@ -119,10 +149,16 @@ def to_do_list_remove_task(username):
         text = listen()
         num = re.search('\d+', text)
         if num:
-            remove_from_list(username, num.group(0))
-
+            remove_from_list(username, int(num.group(0)))
+            
     except Exception as e:
         print(e)
         speak('Sorry unable to remove task')
 
-to_do_list_add('Mihir')
+# add_to_list('Mihir', 'drink water', '27th april 2022', 'monday', '8 a.m.')
+# add_to_list('Mihir', 'complete mini project', '2nd may 2022', '23rd april', ' 11 1 p.m.')
+add_to_list('Mihir', 'watch movie', 'No due date', 'dont remind', 'dont remind')
+
+# to_do_list_remove_task('Mihir')
+
+# to_do_list_open('Mihir')
